dfbe8d8363b45e04fabaaac439af5699da08e7d6
Jacob Sills <jsills2592@gmail.com>
2024-08-15 19:03:30 +0000

    fix: trying to update script for tests that won't pass...still not working

--- number_guess.sh
+++ number_guess.sh
@@ -8,64 +8,58 @@ SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 echo -e "Enter your username:"
 read USERNAME
 
-# Query the database to find the user's information (by username)
-USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+# Query the database to find the user's ID (or check if they exist)
+USER_EXISTS=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
 
 # Check if the user exists in the database
-if [[ -z $USER_INFO ]]
+if [[ -z $USER_EXISTS ]]
 then
   # User doesn't exist
   echo -e "Welcome, $USERNAME! It looks like this is your first time here."
-  
-  # Initialize user's game stats
-  GAMES_PLAYED=0
-  BEST_GAME=0
+  # Insert new user into the database
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
 else
   # User exists
-  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
+  BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
   echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-# Start guessing game
-NUMBER_OF_GUESSES=0
-while true
-do
+# Initialize guess count
+GUESS_COUNT=0
+
+# Game loop
+while true; do
+  # Prompt for guess
   echo -e "Guess the secret number between 1 and 1000:"
   read GUESS
-  
-  # Validate input
-  if [[ ! $GUESS =~ ^[0-9]+$ ]]
-  then
+
+  # Check if the input is an integer
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
     echo -e "That is not an integer, guess again:"
     continue
   fi
-  
-  NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES + 1))
-  
-  # Check the guess
-  if [[ $GUESS -lt $SECRET_NUMBER ]]
-  then
+
+  # Increment guess count
+  GUESS_COUNT=$(( GUESS_COUNT + 1 ))
+
+  # Check guess against the secret number
+  if [[ $GUESS -lt $SECRET_NUMBER ]]; then
     echo -e "It's higher than that, guess again:"
-  elif [[ $GUESS -gt $SECRET_NUMBER ]]
-  then
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]; then
     echo -e "It's lower than that, guess again:"
   else
-    echo -e "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-    
-    # Update the database with game results
-    if [[ $GAMES_PLAYED -eq 0 || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
-    then
-      BEST_GAME=$NUMBER_OF_GUESSES
-    fi
-    
-    GAMES_PLAYED=$((GAMES_PLAYED + 1))
-    
-    # Update user stats in the database
-    $PSQL "UPDATE users SET games_played = $GAMES_PLAYED, best_game = $BEST_GAME WHERE username = '$USERNAME'"
-    
-    # Insert the guess into the guesses table
-    $PSQL "INSERT INTO guesses (username, secret_number, guess_number) VALUES ('$USERNAME', $SECRET_NUMBER, $NUMBER_OF_GUESSES)"
-    
-    break
+    # User guessed the number correctly
+    echo -e "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+    # Insert the guess into the database
+    INSERT_GUESS_RESULT=$($PSQL "INSERT INTO guesses (username, secret_number, guess_number) VALUES ('$USERNAME', '$SECRET_NUMBER', '$GUESS_COUNT')")
+
+    # Update user information
+    # Update games played
+    UPDATE_USER_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1, best_game = LEAST(COALESCE(best_game, $GUESS_COUNT), $GUESS_COUNT) WHERE username = '$USERNAME'")
+
+    # Exit the script
+    exit
   fi
 done
