03f2a51f0c3ac453a02ba4657246a3f158fbebf1
Jacob Sills <jsills2592@gmail.com>
2024-08-15 18:09:17 +0000

    feat: added random number generator between 1 and 1000

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,7 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo -e "Enter your username:"
+read USERNAME
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+echo "The secret number is: $SECRET_NUMBER"
